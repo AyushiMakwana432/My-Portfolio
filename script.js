5b3e7dbbcab2285fd94e7af088543c1d6360970c
AyushiMakwana432 <ayushimakwana432@gmail.com>
2025-01-13 13:35:48 +0530

    a

--- script.js
+++ script.js
@@ -46,14 +46,35 @@ darkModeIcon.onclick = () => {
     document.body.classList.toggle('dark-mode');
 };
 
+function gotowhasapp(){
+    var name = document.getElementById("Full Name").value;
+    var email = document.getElementById("Email Address").value;
+    var sub = document.getElementById("Mobile Number").value;
+    var msg = document.getElementById("Email Subject").value;
+
+    alert(name);
+
+    var url = "https://whatsapp.com/dl/+919712027090"
+    + "Name: " + name + "%0a"
+    + "Email: " + email + "%0a"
+    + "Subject: " + sub + "%0a"
+    + "Message: " + msg;
+    window.open(url).focus();
+}
+  
+
 
 // //  scroll reveal //
 
-// ScrollReveal({ 
-//     reset: true,
-//     distance: '80px',
-//     duration: 2000,
-//     delay: 200
-// });
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
 
-// ScrollReveal().reveal();
+ScrollReveal().reveal('.home-content, .heading',{origin:'top'});
+ScrollReveal().reveal('.home-img img, .Skills, .education, .contact',{ origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p .about-content',{ origin: 'right' });
+
